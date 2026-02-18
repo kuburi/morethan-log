3561e52c9a17210dbd72e9100133a716cb9ebf24
HAN SEUNG YOON <dev@kuburi.kr>
2026-02-19 01:39:11 +0900

    Update Notion API base URL in getRecordMap

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -2,7 +2,7 @@ import { NotionAPI } from "notion-client"
 
 export const getRecordMap = async (pageId: string) => {
   const api = new NotionAPI({
-apiBaseUrl: "https://tiny-cucumber-590.notion.site/api/v3" //your domain
+apiBaseUrl: "https://kuburi.notion.site/api/v3" //your domain
 })
   const recordMap = await api.getPage(pageId)
   return recordMap
