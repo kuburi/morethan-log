98c7837e8c7ee0949372259f87ba897e2c2b9c4e
HAN SEUNG YOON <dev@kuburi.kr>
2026-02-19 01:34:30 +0900

    Configure NotionAPI with custom apiBaseUrl

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -1,7 +1,9 @@
 import { NotionAPI } from "notion-client"
 
 export const getRecordMap = async (pageId: string) => {
-  const api = new NotionAPI()
+  const api = new NotionAPI({
+apiBaseUrl: "https://tiny-cucumber-590.notion.site/api/v3" //your domain
+})
   const recordMap = await api.getPage(pageId)
   return recordMap
 }
